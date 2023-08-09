1faaa5732d018a54ec594a6169cfc7194d08441a
Natallia Langerbeins <natallia.langerbeins@gmail.com>
2023-08-09 23:05:03 +0200

    added a form

--- js/index.js
+++ js/index.js
@@ -11,8 +11,41 @@ const skillsSection = document.querySelector('#skills');
 const skillsList = skillsSection.querySelector('ul');
 
 for (let i = 0; i < skills.length; i++) {
-    const skill = document.createElement('li');
-    skill.innerText = skills[i];
-    skillsList.appendChild(skill);
+  const skill = document.createElement('li');
+  skill.innerText = skills[i];
+  skillsList.appendChild(skill);
 }
 
+const messageForm = document.querySelector('[name="leave_message"]');
+
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const usersName = e.target.usersName.value;
+  const usersEmail = e.target.usersEmail.value;
+  const usersMessage = e.target.usersMessage.value;
+  console.log(usersName);
+  console.log(usersEmail);
+  console.log(usersMessage);
+
+  const messageSection = document.querySelector('#messages');
+  const messageList = messageSection.querySelector('ul');
+  const newMessage = document.createElement('li');
+
+  newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a>
+    wrote: <span>${usersMessage}</span>
+  `;
+
+  const removeButton = document.createElement('button');
+  removeButton.innerText = 'remove';
+  removeButton.setAttribute('type', 'button');
+  removeButton.addEventListener('click', () => {
+    const entry = removeButton.parentNode;
+    entry.remove();
+  });
+
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+  console.log(messageSection);
+  e.target.reset();
+});
